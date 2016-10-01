16e3340b133675ccf4bcb065d448fa69c87aa860
a20092101 <a20092101@V20206.lab.inf.pucp.edu.pe>
2016-10-01 13:38:17 -0500

    Lectura de rutas prhibida y filtrados

--- DataAleatoria/src/dp1_grupo8/clases/Combinacion.java
+++ DataAleatoria/src/dp1_grupo8/clases/Combinacion.java
@@ -5,6 +5,12 @@
  */
 package dp1_grupo8.clases;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+
 /**
  *
  * @author a20092101
@@ -15,5 +21,30 @@ public class Combinacion {
     public Combinacion(String origen, String destino){
         this.origen = origen ; 
         this.destino = destino; 
+    }
+    public Combinacion(String ruta){       
+        String[] ss = ruta.split(" ");        
+        String origen = ss[0];
+        String destino = ss[1];
+        this.origen = origen ; 
+        this.destino = destino ; 
+    }
+    
+     public static Vector<Combinacion> leerCombinaciones(String ruta) throws IOException {
+        
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(ruta));
+        } catch (FileNotFoundException ex) {
+            // Logger.getLogger(EjecucionVNS.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        String r;  
+        Vector<Combinacion> res = new Vector<>();
+
+        while((r = in.readLine()) != null){        
+            Combinacion x= new Combinacion(r);            
+            res.add(x);
+        }            
+        return res;
     }
 }
--- DataAleatoria/src/dp1_grupo8/generaciónData/GeneracionDatosAleatorio.java
+++ DataAleatoria/src/dp1_grupo8/generaciónData/GeneracionDatosAleatorio.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import jdk.nashorn.internal.objects.NativeArray;
+
 
 /**
  *
@@ -21,43 +23,72 @@ import java.util.logging.Logger;
  */
 
 public class GeneracionDatosAleatorio {
-    ArrayList<OrdenDeEnvio> permutaciones; 
+    
     
          
     public static void main(String[] args) {         
         try {
             
             Vector<Ciudad> ciudades =  Ciudad.leerCiudades("./_aeropuertos.OACI.txt");
-            /*for( int i =0; i< ciudades.size() ; i++) {
+            Vector<Combinacion> combinaciones_rutas_prohibidas = Combinacion.leerCombinaciones("./_rutas_prohibidas.txt");   
+            Vector<Combinacion> combinaciones_rutas_filtradas = Permutacion2(ciudades,combinaciones_rutas_prohibidas );   
+            
+           /*
+                for( int i =0; i< combinaciones_rutas_prohibidas.size() ; i++) {
+                 Combinacion comb = combinaciones_rutas_prohibidas.get(i);
+                  System.out.println(comb.origen + " " + comb.destino); 
+                }
+                System.out.println(combinaciones_rutas_prohibidas.size());
+            */
+            /*// Comprobacion lectura ciudades 
+                for( int i =0; i< ciudades.size() ; i++) {
                  Ciudad ciudad_leida = ciudades.get(i);
                   System.out.println(ciudad_leida.codigo); 
              }  */
-             Vector<Combinacion> combinaciones = Permutacion2(ciudades);             
-             /* for( int i =0; i< combinaciones.size() ; i++) {
-                 Combinacion comb = combinaciones.get(i);
+             
+             
+             // Comprobacion de lectura de combinaciones 
+             
+             
+             for( int i =0; i< combinaciones_rutas_filtradas.size() ; i++) {
+                 Combinacion comb = combinaciones_rutas_filtradas.get(i);
                  System.out.println(comb.origen + " " + comb.destino );                   
               }  
-              System.out.println("Cantidad de Combinaciones "+ combinaciones.size());
-             */
+              System.out.println("Cantidad de Combinaciones "+ combinaciones_rutas_filtradas.size());
+             
               
         } catch (IOException ex){                        
-              
+              System.out.println("Error en el Bucle Principal Generación de Data Aleatoria");
         }
     }  
     
-    private static Vector<Combinacion>  Permutacion2(Vector<Ciudad> ciudades) {
+    private static Vector<Combinacion>  Permutacion2(Vector<Ciudad> ciudades, Vector<Combinacion> combinaciones_rutas_prohibidas) {
           Vector<Combinacion> combinaciones = new Vector<Combinacion>();
           Ciudad  ciudad_i = null , ciudad_j = null;
+          String codigo_i, codigo_j ; 
           
           for( int i =0; i< ciudades.size() ; i++) {
                   ciudad_i = ciudades.get(i);
+                  codigo_i = ciudad_i.codigo ; 
                     for( int j =0; j< ciudades.size() ; j++) {
                          ciudad_j = ciudades.get(j);
-                         if (ciudad_i.codigo.compareTo(ciudad_j.codigo) != 0)
-                            combinaciones.add(new Combinacion(ciudad_i.codigo, ciudad_j.codigo));                         
+                         codigo_j = ciudad_j.codigo ;                          
+                         if (codigo_i.compareTo(codigo_j) != 0){
+                             if (!se_encuentra(codigo_i,codigo_j,combinaciones_rutas_prohibidas))
+                                   combinaciones.add(new Combinacion(codigo_i, codigo_j));                         
+                         }
                     }                    
           }  
          return combinaciones;
     }
-    
+    private static boolean se_encuentra(String origen, String destino,Vector<Combinacion> combinaciones_rutas_prohibidas){
+        Combinacion comb ; 
+        for (int i = 0 ; i< combinaciones_rutas_prohibidas.size(); i++ ){
+             comb = combinaciones_rutas_prohibidas.get(i); 
+             if ((origen.compareTo(comb.origen) == 0) && destino.compareTo(comb.destino) == 0) {
+                   return true;                   
+             }
+        }        
+        return false; 
+    }
 }
